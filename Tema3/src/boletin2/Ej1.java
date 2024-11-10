486fb3f3baadb2409a4399b1303ca711c40adfbd
usuario <usuario@DESKTOP-6HJTBHQ.home>
2024-11-10 22:00:11 +0100

    terminar

---
+++ Tema3/src/boletin2/Ej1.java
@@ -0,0 +1,43 @@
+package boletin2;
+
+import java.util.Scanner;
+
+public class Ej1 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Crea un programa que pida al usuario dos valores, tamaño y valor, y luego
+		 * cree un array del tamaño dado y que almacene valor en todas sus posiciones.
+		 * Luego muestra el array por pantalla.
+		 */
+		
+		// variables para almacenar los tamanos
+		int tamano1;
+		int tamano2;
+		
+		// variables para almacenar los valors
+		int valor1;
+		int valor2;
+		
+		// variables para las tablas
+		int[] tabla1;
+		
+		// creamos el escaner
+		Scanner sc = new Scanner (System.in);
+		
+		// pedimos el tamano 1
+		System.out.println("Introduzca el tamano 1:");
+		tamano1 = sc.nextInt();
+		
+		// asignamos el tamano a la tabla 1
+		tabla1[] = new int[tamano1];
+		
+		
+		
+		// cerramos el escaner
+		sc.close();
+
+	}
+
+}
