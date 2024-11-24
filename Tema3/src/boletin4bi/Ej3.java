1b04954383b341bb93cad79a4efdb13248f70345
am-lozano <am.lozano@iesnervion.es>
2024-11-24 21:39:26 +0100

    ejercicio 3 solucionado

--- Tema3/src/boletin4bi/Ej3.java
+++ Tema3/src/boletin4bi/Ej3.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Ej3 {
 	
 	// creamos el escaner para saber las dimensiones de la tabla
-	static Scanner sc = new Scanner (System.in);
-	
+//	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 
@@ -17,44 +16,93 @@ public class Ej3 {
 		 * matriz. Delfine un método main que llame a la función y muestre por pantalla
 		 * el resultado, indicando si la tabla pasada por parámetro es simétrica o no.
 		 */
-		
+
 //		int[][] tabla = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-		int[][] tabla = { { 1, 2,  1, 2 }, { 1, 2,  1, 2 }, { 1, 2,  1, 2 }, { 1, 2,  1, 2 } };
+//		int[][] tabla = { { 2, 2, 2, 2 }, { 2, 2, 2, 2 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
+		int[][] tabla = { { 1, 2, 1, 2 }, { 1, 2, 1, 2 }, { 1, 2, 1, 2 }, { 1, 2, 1, 2 } };
+
+//		System.out.println("Introduzca las dimensiones de la tabla:");
+//		dimensiones = sc.nextInt();
 		
-		// variable para saber las dimensiones de la tabla
-		int dimensiones;
+		System.out.println("Su tabla es: ");
 		
-//		// pedimos el tamanyo de la tabla para saber si es simetrica
-//		System.out.println("Introduzca el tamanyo de la tabla: ");
-		
-		System.out.println("Introduzca las dimensiones de la tabla:");
-		dimensiones = sc.nextInt();
-		
-		// comprobamos el valor devuelto por el metodo "simetria" para saber si la tabla es simetrica
-		System.out.println(simetria(tabla) == dimensiones * 2 ? "La tabla es simetrica" : "La tabla no es simetrica");		
-		
-	}
-	
-	// metodo para averiguar si la tabla es simetrica
-	
-	static int simetria (int[][] t) {
-		
-		int simetrica = 0;
-		
-		// leemos la tabla original y comprobamos si es simetrica -> ( A[i][j] = A[j][i] )
-		for (int i = 0; i < t.length; i++) {
-			for (int j = 0; j < t[i].length; j++) {
-				
-				// comprobamos la simetria de la tabla
-				if (t[i][j] == t[j][i]) {
-					simetrica += 1;
-				}
-				
+		for (int i = 0; i < tabla.length; i++) {
+			for (int j = 0; j < tabla[i].length; j++) {
+				System.out.print(tabla[i][j] + "\t");
 			}
+			System.out.println();
 		}
 		
-		return simetrica;
+		System.out.println();
+
+		// comprobamos el valor devuelto por el metodo "simetria" para saber si la tabla
+		// es simetrica
+		System.out.println(simetria(tabla) ? "Su tabla es simetrica" : "Su tabla no es simetrica");
 		
-	}
+		// cerramos el escaner
+//		sc.close();
 
 	}
+
+	// metodo para averiguar si la tabla es simetrica
+
+	static boolean simetria(int[][] t) {
+
+		// variables para almacenar los valores por la derecha y por la izquierda de la
+		// matriz
+		int inicio;
+		int ultimo;
+		
+		// variable para saber el numero de filas de la matriz
+		int filas = t.length;
+		
+		boolean simetrica = true;
+
+		// leemos la tabla original y comprobamos si es simetrica -> ( A[i][j] = A[j][i]
+		// )
+
+		// leemos la matriz por columnas en ambas direcciones (de inicio a fin y de fin
+		// a inicio simultaneamente) y comparamos ambos valores para saber si son
+		// iguales
+
+		for (int j = 0; j < t[0].length; j++) {
+			for (int i = 0, k = filas - 1; i <= k; i++, k--) {
+				
+				/* almacenamos los valores por la izquierda y por la derecha de la siguiente forma ->
+				 * 
+				 * matriz: (1 2 3 4)
+				 * 		   (1 2 1 2)
+				 * 
+				 * inicio = 1; ultima = 4;
+				 * inicio = 2; ultima = 3;
+				 * 
+				 * inicio = 1; ultima = 4;
+				 * inicio = 2; ultima = 3;
+				 * 
+				 * de esta forma vamos recorriendo la tabla por columnas en ambos sentidos a la vez y comprobando dichos valores para saber si es simetrica
+				 */
+				inicio = t[i][j];
+				ultimo = t[k][j];
+				
+				if (inicio != ultimo)
+					simetrica = false;
+
+			}
+		}
+
+//		for (int i = 0; i < t.length; i++) {
+//			for (int j = 0; j < t[i].length; j++) {
+//				
+//				// comprobamos la simetria de la tabla
+//				if (t[i][j] == t[j][i]) {
+//					simetrica += 1;
+//				}
+//				
+//			}
+//		}
+
+		return simetrica;
+
+	}
+
+}
