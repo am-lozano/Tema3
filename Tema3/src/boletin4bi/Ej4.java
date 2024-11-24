59d20c90787b7c59adc83642ab69032d1462e2bf
am-lozano <am.lozano@iesnervion.es>
2024-11-24 21:59:27 +0100

    corregir 3

--- Tema3/src/boletin4bi/Ej4.java
+++ Tema3/src/boletin4bi/Ej4.java
@@ -16,8 +16,12 @@ public class Ej4 {
 		
 		// variable para almacenar la matriz a comprobar
 //		int[][] tabla = {{4,9,2}, {3,5,7}, {8,1,6}};
-//		int[][] tabla = {{5,10,3}, {4,6,8}, {9,2,7}};
-		int[][] tabla = {{4,8,2}, {6,5,7}, {8,4,6}};
+		int[][] tabla = {{5,10,3}, {4,6,8}, {9,2,7}};
+//		int[][] tabla = {{4,8,2}, {6,5,7}, {8,4,6}};
+		
+		// variables para recoger los valores de los metodos
+		boolean fila = magFila(tabla);
+		boolean col = magCol(tabla);
 		
 		System.out.println("Su matriz es: ");
 		
@@ -33,26 +37,36 @@ public class Ej4 {
 		
 		System.out.println();
 		
-		System.out.println(esMagica(tabla) ? "La matriz es magica" : "La matriz no es magica");
+		// comprobamos los valores de salida de los metodos para saber si laa matriz es magica
+		if (!fila && !col)
+			System.out.println("La matriz es magica");
+		else
+			System.out.println("La matriz no es magica");
 		
 	}
 	
-	static boolean esMagica (int tabla[][]) {
+	// metodo para comprobar las filas
+	static boolean magFila (int tabla[][]) {
 		
-		// variable boolean para saber si la matriz es magica
-		boolean magica = true;
+		boolean enc = false;
 		
-		// variable para la suma de los valores de las columnas o filas de la matriz
+		// variable para la suma de los valores de la tabla
 		int suma = 0;
 		
 		// variable para saber la suma de las filas o columnas
 		int sumaFilaCol = 0;
 		
-		// recorremos la matriz y comprobamos que la suma de la fila o columna es igual a la suma anterior
-		for (int i = 0; i < tabla.length; i++) {
-			for (int j = 0; j < tabla[i].length; j++) {
+		// variables para recorrer la tabla
+		int i = 0;
+		int j = 0;
+		
+		// reocrremos la tabla por filas y sumamos sus valores
+		while (i < tabla.length && !enc) {
+			while (j < tabla[i].length) {
 				
 				suma += tabla[i][j];
+				
+				j++;
 			}
 			
 			if (i == 0)
@@ -60,31 +74,103 @@ public class Ej4 {
 			
 			// comprobamos la suma de las filas de la matriz
 			if (suma != sumaFilaCol)
-				magica = false;
+				enc = true;
 			
 			suma = 0;
+			
+			i++;
 		}
 		
-		for (int j = 0; j < tabla[0].length; j++) {
-			for (int i = 0; i < tabla[j].length; i++) {
-				
-				suma += tabla[j][i];
-			}
-			
-			if (j == 0)
-				sumaFilaCol = suma;
-			
-			// comprobamos la suma de las filas de la matriz
-			if (suma != sumaFilaCol)
-				magica = false;
-			
-			suma = 0;
-		}
-		
-		
-		
-		return magica;
-		
+		return enc;
 	}
+	
+	// metodo para comprobar las columnas
+		static boolean magCol (int tabla[][]) {
+			
+			boolean enc = false;
+			
+			// variable para la suma de los valores de la tabla
+			int suma = 0;
+			
+			// variable para saber la suma de las filas o columnas
+			int sumaFilaCol = 0;
+			
+			// variables para recorrer la tabla
+			int i = 0;
+			int j = 0;
+			
+			// reocrremos la tabla por filas y sumamos sus valores
+//			while (j < tabla[0].length && !enc) {
+//				while (i < tabla[j].length) {
+//					
+//					suma += tabla[j][i];
+//					
+//					j++;
+//				}
+//				
+//				if (j == 0)
+//					sumaFilaCol = suma;
+//				
+//				// comprobamos la suma de las filas de la matriz
+//				if (suma != sumaFilaCol)
+//					enc = true;
+//				
+//				suma = 0;
+//				
+//				i++;
+//			}
+			
+			return enc;
+		}
+	
+//	static boolean esMagica (int tabla[][]) {
+//		
+//		// variable boolean para saber si la matriz es magica
+//		boolean magica = true;
+//		
+//		// variable para la suma de los valores de las columnas o filas de la matriz
+//		int suma = 0;
+//		
+//		// variable para saber la suma de las filas o columnas
+//		int sumaFilaCol = 0;
+//		
+//		// recorremos la matriz y comprobamos que la suma de la fila o columna es igual a la suma anterior
+//		for (int i = 0; i < tabla.length; i++) {
+//			for (int j = 0; j < tabla[i].length; j++) {
+//				
+//				suma += tabla[i][j];
+//			}
+//			
+//			if (i == 0)
+//				sumaFilaCol = suma;
+//			
+//			// comprobamos la suma de las filas de la matriz
+//			if (suma != sumaFilaCol)
+//				magica = false;
+//			
+//			suma = 0;
+//		}
+//		
+//		for (int j = 0; j < tabla[0].length; j++) {
+//			for (int i = 0; i < tabla[j].length; i++) {
+//				
+//				suma += tabla[j][i];
+//			}
+//			
+//			if (j == 0)
+//				sumaFilaCol = suma;
+//			
+//			// comprobamos la suma de las filas de la matriz
+//			if (suma != sumaFilaCol)
+//				magica = false;
+//			
+//			suma = 0;
+//		}
+//		
+//		
+//		
+//		return magica;
+//		
+//	}
 
 }
